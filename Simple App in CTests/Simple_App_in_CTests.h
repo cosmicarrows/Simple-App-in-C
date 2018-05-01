58cdab0280ad2827008619deb69ee1fa78982dab
Cosmic Arrows <cosmicarrowsllc@Cosmics-MacBook-Pro.local>
2018-05-01 16:56:33 -0400

    Upload assets to hardrive

---
+++ Simple App in CTests/Simple_App_in_CTests.h
@@ -0,0 +1,13 @@
+//
+//  Simple_App_in_CTests.h
+//  Simple App in CTests
+//
+//  Created by Laurence Wingo on 10/22/12.
+//  Copyright (c) 2012 Laurence Wingo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Simple_App_in_CTests : SenTestCase
+
+@end
---
+++ Simple App in CTests/Simple_App_in_CTests.m
@@ -0,0 +1,32 @@
+//
+//  Simple_App_in_CTests.m
+//  Simple App in CTests
+//
+//  Created by Laurence Wingo on 10/22/12.
+//  Copyright (c) 2012 Laurence Wingo. All rights reserved.
+//
+
+#import "Simple_App_in_CTests.h"
+
+@implementation Simple_App_in_CTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Simple App in CTests");
+}
+
+@end
